500331e53304ff65bf2f279e65f7f4320cf463ac
Kim Yeongjin <inusneo@gmail.com>
2023-02-02 14:48:29 +0900

    [edit] js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const todoInput = document.querySelector('#todoInput');
 
-const keyCode = () => {
-  if (window.event.keyCode === 13 && todoInput.value.trim() !== '') {
-    const todoList = document.querySelector('#todoList');
-    const addLi = document.createElement('li');
-    const addSpan = document.createElement('span');
+const createTodo = () => {
+  const todoList = document.querySelector('#todoList');
+  const addLi = document.createElement('li');
+  const addSpan = document.createElement('span');
 
-    addSpan.textContent = todoInput.value;
-    addLi.appendChild(addSpan);
-    todoList.appendChild(addLi);
-    todoInput.value = '';
+  addSpan.textContent = todoInput.value;
+  addLi.appendChild(addSpan);
+  todoList.appendChild(addLi);
+  todoInput.value = '';
+}
+
+const keyCodeChk = () => {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== '') {
+    createTodo();
   }
 }
