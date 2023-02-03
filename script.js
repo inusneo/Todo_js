38aa5396a8a05062f7fd8e99b7709975d7032446
Kim Yeongjin <inusneo@gmail.com>
2023-02-03 16:55:01 +0900

    [add] delete function

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ const createTodo = () => {
   addChk.addEventListener('click', () => {
     addLi.classList.toggle('complete');
   });
+  addLi.addEventListener('dblclick', () => {
+    addLi.remove();
+  });
+
   addSpan.textContent = todoInput.value;
   addLi.appendChild(addChk);
   addLi.appendChild(addSpan);
@@ -20,5 +24,12 @@ const createTodo = () => {
 const keyCodeChk = () => {
   if (window.event.keyCode === 13 && todoInput.value.trim() !== '') {
     createTodo();
+  }
+}
+
+const delAll = () => {
+  const liList = document.querySelectorAll('li');
+  for (let idx of liList) {
+    idx.remove();
   }
 }
